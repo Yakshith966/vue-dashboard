fad753bc7df6751b5352c99683a9bedb5da69606
chethan shetty <chethanshetty741@gmail.com>
2024-02-06 11:07:40 +0530

    products side nav added

--- vue-argon-dashboard-main/src/views/components/NewsCard.vue
+++ vue-argon-dashboard-main/src/views/components/NewsCard.vue
@@ -1,247 +1,168 @@
 
 <template>
-    <div class="card mb-4">
-      <div class="card-header pb-0">
-        <h6>News</h6>
-        <div class="col-5 w-95 text-end mb-4">
-        <argon-button color="dark"  variant="gradient">
+  <div class="card mb-4">
+    <div class="card-header pb-0">
+      <h6>News</h6>
+      <div class="col-5 w-95 text-end mb-4">
+        <argon-button color="dark" variant="gradient">
           <i class="fas fa-plus me-2"></i>
           Add
         </argon-button>
       </div>
-        
-      </div>
-      <div class="card-body pt-3 p-3 w-100">
-        <ul
-          class="list-group d-flex flex-row flex-wrap gap-5 w-100 justify-content-center m-auto"
-        >
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-          <li
-            class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg"
-          >
-            <div class="d-flex flex-column">
-              
-              <img
-                src="../../assets/img/bg-profile.jpg"
-                alt="Image placeholder"
-                class="card-img-top w-100 border-radius-lg"
-              />
-              <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
-              <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum.</p>
-            </div>
-           
-            <div class="ms-auto pt-2 pb--3">
-                <a
-                class="btn btn-link text-dark text-gradient px-3"
-                href="javascript:;"
-              >
-                <i  class="fas fa-pencil-alt text-dark me-2"
-                      aria-hidden="true"></i>Edit
-              </a>
-                
-              <a
-                class="btn btn-link text-danger text-gradient px-3"
-                href="javascript:;"
-              >
-                <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
+
     </div>
-  </template>
-  <script>
-  import ArgonButton from "@/components/ArgonButton.vue";
-  export default {
-    components:{
-      ArgonButton
-    },
-    name: "projects-table",
-  };
-  </script>
+    <div class="card-body pt-3 p-3 w-100">
+      <ul class="list-group d-flex flex-row flex-wrap gap-5 w-100 justify-content-center m-auto">
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+        <li class="list-group-item border-0 d-flex flex-column col-md-3 mb-3 bg-gray-100 border-radius-lg">
+          <div class="d-flex flex-column">
+
+            <img src="../../assets/img/bg-profile.jpg" alt="Image placeholder"
+              class="card-img-top w-100 border-radius-lg" />
+            <h6 class="pr-3 pt-2 text-sm">Breaking News</h6>
+            <p class="pr-3 pt-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio
+              ipsum.</p>
+          </div>
+
+          <div class="ms-auto pt-2 pb--3">
+            <a class="btn btn-link text-dark text-gradient px-3" href="javascript:;">
+              <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
+            </a>
+
+            <a class="btn btn-link text-danger text-gradient px-3" href="javascript:;">
+              <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+import ArgonButton from "@/components/ArgonButton.vue";
+export default {
+  components: {
+    ArgonButton
+  },
+  name: "projects-table",
+};
+</script>
